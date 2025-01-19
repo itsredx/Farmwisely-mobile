5ed8620c1ff93f846734e4650868d7008ca05831
ambashir02 <ambashir02@gmail.com>
2025-01-19 23:23:06 +0000

    Chat no request

--- lib/screens/ai_chat.dart
+++ lib/screens/ai_chat.dart
@@ -1,202 +1,3 @@
-/*
-import 'package:farmwisely/utils/colors.dart';
-import 'package:flutter/material.dart';
-
-class AiChatScreen extends StatelessWidget {
-  const AiChatScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.background,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Ai Farm Advisor',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Ask me anything about your farm',
-              style: TextStyle(fontSize: 12, color: AppColors.grey),
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.history),
-          ),
-        ],
-        backgroundColor: AppColors.background,
-      ),
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 160.0),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: double.infinity,
-                    width: double.infinity,
-                  ),
-                  Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: AppColors.primary,
-                      ),
-                      width: 300,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              'Chat Screen',
-                              style: TextStyle(fontSize: 24),
-                            ),
-                            SizedBox(
-                              height: 16,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Card(
-                                    color: AppColors.secondary,
-                                    surfaceTintColor: AppColors.secondary,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        'How to grow tomato?',
-                                        style: TextStyle(fontSize: 16),
-                                        maxLines: 2,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Card(
-                                    color: AppColors.secondary,
-                                    surfaceTintColor: AppColors.secondary,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        'How to grow beans?',
-                                        style: TextStyle(fontSize: 16),
-                                        maxLines: 2,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Card(
-                                    color: AppColors.secondary,
-                                    surfaceTintColor: AppColors.secondary,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        'How to grow maize?',
-                                        style: TextStyle(fontSize: 16),
-                                        maxLines: 2,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Card(
-                                    color: AppColors.secondary,
-                                    surfaceTintColor: AppColors.secondary,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        'How to grow mango?',
-                                        style: TextStyle(fontSize: 16),
-                                        maxLines: 2,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: double.infinity,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-            left: 0,
-            right: 0,
-            child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 18.0),
-                child: Material(
-                  elevation: 2,
-                  borderRadius: BorderRadius.circular(24),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Type your message...',
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.send),
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 12.0,
-                        horizontal: 16.0,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(24),
-                        borderSide: BorderSide.none,
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-*/
 import 'package:farmwisely/utils/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -244,6 +45,9 @@ class AiChatScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 16.0),
                 child: Column(
                   children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).viewInsets.bottom > 0 ? 10 : 140,
+                    ),
                     Center(
                       child: Container(
                         decoration: BoxDecoration(
@@ -362,8 +166,19 @@ class AiChatScreen extends StatelessWidget {
                     hintText: 'Type your message...',
                     suffixIcon: IconButton(
                       onPressed: () {},
-                      icon: const Icon(Icons.send),
+                      icon: const Icon(
+                        Icons.send,
+                        color: AppColors.secondary,
+                        size: 30,
+                      ),
                     ),
+                    /*prefixIcon: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.mic,
+                        color: AppColors.primary,
+                        ),
+                    ),*/
                     contentPadding: const EdgeInsets.symmetric(
                       vertical: 12.0,
                       horizontal: 16.0,
@@ -373,7 +188,7 @@ class AiChatScreen extends StatelessWidget {
                       borderSide: BorderSide.none,
                     ),
                     filled: true,
-                    fillColor: Colors.white,
+                    fillColor: AppColors.primary,
                   ),
                 ),
               ),
